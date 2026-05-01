b887e9793afb65ae0e6b1964c64f595038e43ce9
Abhijith06546 <abhijithsajju@gmail.com>
2026-05-01 12:12:18 +0530

    working model

---
+++ lib/zego_sdk_key_center.dart
@@ -0,0 +1,5 @@
+class SDKKeyCenter {
+  static const int appID = 1537619792;
+  static const String serverSecret = 'eeb701fb8ae0c22846addef4ec5d1fd8';
+  static const String appSign ='cb6372fef38be6b9eb7eed4e894ec0ef0a0d52a710fd1caf19381727fb3fb1ea' ;
+}
